76d0296405b3294b16a5bd3a319d6de8c2c93d17
iam844 <jaiswaladitya844@gmail.com>
2018-10-30 17:53:53 +0530

    Add Firebase Auth and Database

--- app/src/main/java/com/iam844/adityajaiswal/noteit/MainActivity.java
+++ app/src/main/java/com/iam844/adityajaiswal/noteit/MainActivity.java
@@ -1,13 +1,40 @@
 package com.iam844.adityajaiswal.noteit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        //If user is there then login else open Registration page
+        updateUI();
+    }
+
+
+    private  void updateUI() {
+
+        if(fAuth.getCurrentUser() != null) {
+            Log.i("Main Activity", "fAuth != null");
+        }   else {
+            Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
+            startActivity(startIntent);
+
+            finish();
+
+            Log.i("Main Activity", "fAuth == null");
+        }
+
     }
 }
---
+++ app/src/main/java/com/iam844/adityajaiswal/noteit/StartActivity.java
@@ -0,0 +1,72 @@
+package com.iam844.adityajaiswal.noteit;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.iam844.adityajaiswal.noteit.user_sign.RegisterActivity;
+
+public class StartActivity extends AppCompatActivity {
+
+    private Button btnReg, btnLog;
+
+    private FirebaseAuth fAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        btnReg = findViewById(R.id.start_reg_btn);
+        btnLog = findViewById(R.id.start_log_btn);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        updateUI();
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
+
+        btnLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+
+    }
+
+    private void register() {
+
+        Intent regIntent = new Intent(StartActivity.this, RegisterActivity.class);
+        startActivity(regIntent);
+
+    }
+
+    private void login() {
+
+    }
+
+    private  void updateUI() {
+
+        if (fAuth.getCurrentUser() != null) {
+
+            Log.i("Start Activity", "fAuth != null");
+
+            Intent startIntent = new Intent(StartActivity.this, MainActivity.class);
+            startActivity(startIntent);
+
+            finish();
+        } else {
+            Log.i("Start Activity", "fAuth == null");
+        }
+    }
+}
